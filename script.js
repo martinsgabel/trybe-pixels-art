a1f80c3b653b67f98836a6c096813390425f3391
martins <gabrielamspinola@gmail.com>
2022-01-06 10:41:53 -0300

    Mais Requisitos

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-let pixelBoard = document.querySelector('.pixel-board');
-let rowNum = 5;
+let pixelBoard = document.querySelector('#pixel-board');
 
-//descobri como adicionar uma classe juntamente com o elemento com a ajuda de uma resposta desta fonte: https://stackoverflow.com/questions/55224589/document-createelementdiv-with-a-class
-for (let i = 0; i <= rowNum; i += 1) {
-    for (let x = 0; x <= rowNum; x += 1) {
+//descobri como adicionar uma classe juntamente com o elemento com a ajuda de uma resposta desta fonte: https://stackoverflow.com/questions/55224589/document-createelementdiv-with-a-class 
+for (let i = 0; i < 5; i += 1) {
+    for (let x = 0; x < 5; x += 1) {
         let pixelUnit = document.createElement('div');
         pixelUnit.classList.add('pixel');
         
-        //pixelBoard.appendChild(pixelUnit);
+        pixelBoard.appendChild(pixelUnit);
     }
 }
 
@@ -16,3 +15,11 @@ let firstSelected = document.querySelector('.a');
 firstSelected.classList.add('selected');
 
 //escutador de Eventos, clique na cor
+let clickedColor = document.getElementsByClassName("color");
+
+clickedColor.addEventListener("click", recebeClick);
+
+function recebeClick() {
+    let firstSelected = document.querySelector('.a');
+    firstSelected.classList.add('selected');
+}
